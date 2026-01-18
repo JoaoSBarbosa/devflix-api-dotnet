cc5a656662057a4e04684ffaf830e908ea940058
João - Windows <contato.jsbarbosa@gmail.com>
2026-01-18 16:05:24 -0300

    Implementação basica do teste de instanciação de categoria; criação da entidade categoria

---
+++ src/DevFlix.Domain/Entities/Category.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace DevFlix.Domain.Entities
+{
+    public class Category
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; } = string.Empty;
+        public string Description { get; private set; } = string.Empty;
+
+        public bool IsActive { get; private set; } = true;
+        public DateTime CreateDateTime { get; private set; } = DateTime.UtcNow;
+
+        public Category(string name, string description)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Description = description;
+        }
+    }
+}
